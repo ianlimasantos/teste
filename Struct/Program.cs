f59b24c21282570be37a392b36eb1daac67f7b37
Ian <ianbernardolim@gmail.com>
2023-01-22 14:58:01 -0300

    Branch Cristal uiii

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -24,6 +24,9 @@
         //TECNAAAAAAAAAAAAA
         //FLORAAAAAAAAAAA
         //joelma
+        // AAAAAAAAAAAAAAAAAAA
+        // AMO ANAVITORIAAAAAAAAAAAAAAA
+        // DANCO COMO UM PASTELLLL
         for (int i = 0; i < 2; i++)
         {
             Console.WriteLine("Digite o nome:");
